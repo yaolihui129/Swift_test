116a349a8b357e3977e4f93f7b64e302ffb37134
yaolihui129 <yaolihui129@sina.com>
2019-07-30 14:54:38 +0800

    test1

---
+++ day01/Myt1.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+//
+//var str = "Hello, playground"
+//
+//print(str)
+//
+//let NAME:String = "yaolh"
+//print(NAME)
+//var age:Int = 35
+//print(age)
+//
+//let money = 100
+//let price = 70
+//if !(money >= price) {
+//    print("Buy it")
+//}
+//else{
+//    print("Can not buy it")
+//}
+
+//for index in 1..<10{
+//    print(index)
+//}
+
+//for i in -99...99{
+//    i*i
+//}
+var result = 1
+var base = 2
+var power = 0
+for _ in 1...power{
+    
+}
